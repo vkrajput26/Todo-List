4141d8eef4c1b16502daafcfa543f71499e4bac0
Vivek Rajput <indvivekrajput@gmail.com>
2022-07-30 23:43:31 +0530

    adding todo files

---
+++ vite-project/src/Components/Todo.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import React from "react"
+import TodoList from "./TodoList"
+
+
+export default function Todo(){
+
+    const [text, setText]=useState("")
+    const [data,setData]=useState([])
+    
+    const handleInput=(e)=>{
+        setText(e.target.value)
+    }
+
+    const handleAdd=()=>{
+        setData([...data,text])
+    }
+ console.log("data",data)
+    console.log(text)
+    return(
+        <div>
+            <input type={text} value={text} onChange={handleInput} />
+        <button onClick={handleAdd}>Add</button>
+        <TodoList data={data}/>
+        </div>
+    )
+}
---
+++ vite-project/src/Components/TodoList.jsx
@@ -0,0 +1,17 @@
+
+
+export default function TodoList({data}){
+ 
+    console.log("todo",data)
+    return (
+        <div style={{border:"1px solid black" ,marginTop:"3rem"}}>
+            {
+                data.map((item)=>(
+                <div style={{display:"flex",marginTop:"1rem", justifyContent:"space-around"}}>
+                    <h1>{item}</h1>
+                    <button>Delete</button>
+                </div>))
+            }
+        </div>
+    )
+}
